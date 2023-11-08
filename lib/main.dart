d1fa0e3b4e968baf498ee4db7cb618216700f72f
Seng Wei Han <sengweihan@metabit.network>
2023-11-08 11:52:56 +0800

    feat: create a new home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:simple_app/page/home.dart';
 
 // Declaring a provider called nameProvider which will hold a state that a consumer (entire widget in the application) can access
 final counterProvider = StateProvider((ref) => 0);
@@ -17,56 +18,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Welcome to my page")),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Go to counter page"),
-          onPressed: () {
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: ((context) => const CounterPage())));
-          },
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: const HomePage());
   }
 }
 
-class CounterPage extends ConsumerWidget {
-  const CounterPage({super.key});
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(counterProvider);
-    return Scaffold(
-      body: Center(
-        child: Text(
-          counter.toString(),
-          style: const TextStyle(color: Colors.black),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          ref.read(counterProvider.notifier).state++;
-        },
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text("Welcome to my page")),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: const Text("Go to counter page"),
+//           onPressed: () {
+//             Navigator.of(context).push(
+//                 MaterialPageRoute(builder: ((context) => const CounterPage())));
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class CounterPage extends ConsumerWidget {
+//   const CounterPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final counter = ref.watch(counterProvider);
+//     return Scaffold(
+//       body: Center(
+//         child: Text(
+//           counter.toString(),
+//           style: const TextStyle(color: Colors.black),
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: const Icon(Icons.add),
+//         onPressed: () {
+//           ref.read(counterProvider.notifier).state++;
+//         },
+//       ),
+//     );
+//   }
+// }
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+/*
+This is a stateless widget for the home screen.
+
+*/
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(body: Text("hello world"));
   }
 }
